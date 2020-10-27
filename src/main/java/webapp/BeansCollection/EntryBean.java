73e0a74e53b20465fd25a5cc4d4e9ed745c81d33
HusseinHalifaev <HusseinHalifaev@github.com>
2020-10-27 22:07:57 +0300

    beanCollections

---
+++ src/main/java/webapp/BeansCollection/EntryBean.java
@@ -0,0 +1,32 @@
+package webapp.BeansCollection;
+
+import webapp.RequestBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+@SessionScoped
+@ManagedBean(name = "entryBean")
+public class EntryBean {
+    private final List<RequestBean> dataList = new ArrayList<>();
+
+    @Inject
+    public RequestBean bean;
+
+    public void addToResultList(RequestBean bean){
+        this.dataList.add(0, bean);
+    }
+
+    public RequestBean selectFromResultBean(){
+        return dataList.get(0);
+    }
+
+    public List<RequestBean> getDataList() {
+        return dataList;
+    }
+}
