3724e991c61b31e4de5943fdc05939863a072be6
HusseinHalifaev <HusseinHalifaev@github.com>
2020-11-13 18:33:36 +0300

    Да простит меня Бог за то говно которое я тут написал

--- src/main/webapp/js/response.js
+++ src/main/webapp/js/response.js
@@ -1,3 +1,39 @@
-function setX(x){
-    document.getElementById('_form:x_value').setAttribute('value',x);
+function setX(name_variable, value_variable) {
+    if (name_variable === 'x') {
+        document.getElementById('_form:x_value').setAttribute('value', value_variable);
+        for (let i = 16; i <= 22; i++) {
+            if(i!==value_variable+19) {
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
+            }else {
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-check');
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default ui-state-active');
+            }
+        }
+    }
+    if (name_variable === 'y') {
+        document.getElementById('_form:y_value').setAttribute('value', value_variable)
+        //ну тут я считаю пздц какой-то
+        var number;
+        if(value_variable===1){
+            number=26;
+        }if(value_variable===1.5){
+            number=27;
+        }if (value_variable===2){
+            number=28;
+        }if (value_variable===2.5){
+            number=29;
+        }if (value_variable===3){
+            number=30;
+        }
+        for (let i = 26; i <= 30; i++) {
+            if(i!==number) {
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
+            }else {
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-check');
+                document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default ui-state-active');
+            }
+        }
+    }
 }
