41f0765d838fa9a46ec8778802839d37a252b670
HusseinHalifaev <HusseinHalifaev@github.com>
2020-11-14 18:18:00 +0300

    commit

--- target/web_3-1.0-SNAPSHOT/js/response.js
+++ target/web_3-1.0-SNAPSHOT/js/response.js
@@ -1,14 +1,16 @@
 let radiusValue;
+let x_is_selected = false;
+let r_is_selected = false;
 
 
 function setValue(name_variable, value_variable) {
     if (name_variable === 'x') {
         document.getElementById('_form:x_value').setAttribute('value', value_variable);
         for (let i = 16; i <= 22; i++) {
-            if(i!==value_variable+19) {
+            if (i !== value_variable + 19) {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
-            }else {
+            } else {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-check');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default ui-state-active');
             }
@@ -19,25 +21,35 @@ function setValue(name_variable, value_variable) {
         //ну тут я считаю пздц какой-то
         radiusValue = value_variable;
         var number;
-        if(value_variable===1){
-            number=26;
-        }if(value_variable===1.5){
-            number=27;
-        }if (value_variable===2){
-            number=28;
-        }if (value_variable===2.5){
-            number=29;
-        }if (value_variable===3){
-            number=30;
+        if (value_variable === 1) {
+            number = 26;
+        }
+        if (value_variable === 1.5) {
+            number = 27;
+        }
+        if (value_variable === 2) {
+            number = 28;
+        }
+        if (value_variable === 2.5) {
+            number = 29;
+        }
+        if (value_variable === 3) {
+            number = 30;
         }
         for (let i = 26; i <= 30; i++) {
-            if(i!==number) {
+            if (i !== number) {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
-            }else {
+            } else {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-check');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default ui-state-active');
             }
         }
+    }
+}
+
+function validator() {
+    if ((x_is_selected && r_is_selected) === false){
+        alert("error")
     }
 }
