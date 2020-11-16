6cae7b016a3c68a2ce43ba5ced59b2034e9d1363
HusseinHalifaev <HusseinHalifaev@github.com>
2020-11-16 23:26:09 +0300

    commit

--- src/main/webapp/js/coordinateDebilder.js
+++ src/main/webapp/js/coordinateDebilder.js
@@ -49,13 +49,13 @@ $(function () {
     $("#checkbox_r").on("click", function () {
         let half_value = String(Number(radiusValue) / 2);
         document.querySelector('#radius-text-right').innerHTML = radiusValue;
-        document.querySelector('#radius-text-left').innerHTML = radiusValue;
+        document.querySelector('#radius-text-left').innerHTML = "-"+radiusValue;
         document.querySelector('#radius-text-top').innerHTML = radiusValue;
-        document.querySelector('#radius-text-bottom').innerHTML = radiusValue;
+        document.querySelector('#radius-text-bottom').innerHTML = "-"+radiusValue;
         document.querySelector('#radius-text-right-half').innerHTML = half_value;
-        document.querySelector('#radius-text-left-half').innerHTML = half_value;
+        document.querySelector('#radius-text-left-half').innerHTML = "-"+half_value;
         document.querySelector('#radius-text-top-half').innerHTML = half_value;
-        document.querySelector('#radius-text-bottom-half').innerHTML = half_value;
+        document.querySelector('#radius-text-bottom-half').innerHTML = "-"+half_value;
         return 0;
     });
 });
