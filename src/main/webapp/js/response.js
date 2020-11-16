1d05b49e694064a38b9df9bbad86a5ffb9f23a81
HusseinHalifaev <HusseinHalifaev@github.com>
2020-11-16 23:52:43 +0300

    commit

--- src/main/webapp/js/response.js
+++ src/main/webapp/js/response.js
@@ -7,8 +7,8 @@ function setValue(name_variable, value_variable) {
     if (name_variable === 'x') {
         document.getElementById('_form:x_value').setAttribute('value', value_variable);
         x_value=true;
-        for (let i = 16; i <= 22; i++) {
-            if (i !== value_variable + 19) {
+        for (let i = 17; i <= 23; i++) {
+            if (i !== value_variable + 20) {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
             } else {
@@ -23,21 +23,21 @@ function setValue(name_variable, value_variable) {
         r_value=true;
         var number;
         if (value_variable === 1) {
-            number = 25;
-        }
-        if (value_variable === 1.5) {
             number = 26;
         }
-        if (value_variable === 2) {
+        if (value_variable === 1.5) {
             number = 27;
         }
-        if (value_variable === 2.5) {
+        if (value_variable === 2) {
             number = 28;
         }
-        if (value_variable === 3) {
+        if (value_variable === 2.5) {
             number = 29;
         }
-        for (let i = 25; i <= 29; i++) {
+        if (value_variable === 3) {
+            number = 30;
+        }
+        for (let i = 26; i <= 30; i++) {
             if (i !== number) {
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].children[0].setAttribute('class', '.ui-chkbox-icon ui-icon ui-c ui-icon-blank');
                 document.getElementById('_form:j_idt' + i).getElementsByClassName("ui-chkbox-box ui-widget ui-corner-all ui-state-default")[0].setAttribute('class', 'ui-chkbox-box ui-widget ui-corner-all ui-state-default');
